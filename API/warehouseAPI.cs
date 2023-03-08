7f1cc52103e60f48fedf312e4d6987fda08031d5
Nagendra Mishr <nagendra@microsoft.com>
2023-03-08 05:38:44 -0500

    initial plugin to cosmos

--- API/warehouseAPI.cs
+++ API/warehouseAPI.cs
@@ -15,21 +15,16 @@ namespace API
         [FunctionName("warehouseAPI")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+            [CosmosDB(
+                databaseName: "SP519as4",
+                collectionName: "shipments",
+                ConnectionStringSetting  = "CosmosDBConnection")]
+                IAsyncCollector<Shipment> shipmentsOut,
+                ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return new JsonResult(shipmentsOut);
         }
     }
 }
