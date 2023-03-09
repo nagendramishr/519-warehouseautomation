326bd3707c374ddb25ab1652d0e745d656cf8b0b
Nagendra Mishr <nagendra@microsoft.com>
2023-03-08 19:07:37 -0500

    update settings

--- API/warehouseAPI.cs
+++ API/warehouseAPI.cs
@@ -1,12 +1,13 @@
-using System;
-using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace API
 {
@@ -18,13 +19,13 @@ namespace API
             [CosmosDB(
                 databaseName: "SP519as4",
                 containerName: "shipments",
-                Connection   = "CosmosDBConnection")]
-                IAsyncCollector<Shipment> shipmentsOut,
+                SqlQuery = "SELECT * FROM c",
+                Connection = "CosmosDBConnection")] IEnumerable<Shipment> shipmentsIn,
                 ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            return new JsonResult(shipmentsOut);
+            return new JsonResult(shipmentsIn);     
         }
     }
 }
