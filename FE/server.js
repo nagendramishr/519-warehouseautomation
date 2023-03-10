c3caf55be22ebf8c2880ee44f169162d6e9c7ad9
Nagendra Mishr <nagendra@microsoft.com>
2023-03-10 09:38:26 -0500

    start on PORT env variable

--- FE/server.js
+++ FE/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const unirest = require("unirest");
 
 app.get('/api/getShipment/:shipperID', (req, res) => {
